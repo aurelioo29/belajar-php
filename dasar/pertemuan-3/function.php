a9acaec3366ece886ed283ac92339c71d09fb99f
Haloio29 <aureliolucio5@gmail.com>
2023-10-10 21:30:50 +0700

    add materi function

--- dasar/pertemuan-3/function.php
+++ dasar/pertemuan-3/function.php
@@ -1,1 +1,18 @@
 <?php
+/*
+di function pada parameter nya dapat di isi beberapa argumen.
+dapat diberikan nilai default juga pada argumen nya, cth ⬇️ ;
+// function salam ($nama="Admin"){
+//  return "Halo Selamat Datang $nama, Ada Bisa Dibantu?";
+// }
+// echo salam()
+/////////////////////
+maka output nya akan, dikarenakan paramater tidak di isi & otomatis di isi dengan nilai default
+-------------------------Halo Selamat Datang Admin, Ada Bisa Dibantu?--------------------------
+*/
+
+function salam ($nama){
+  return "Halo Selamat Datang $nama, Ada Bisa Dibantu?";
+}
+echo salam('Aurelio');
+?>
