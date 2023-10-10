ddf572aa02d2fb0bb86c53a60296eced361a6af5
Haloio29 <aureliolucio5@gmail.com>
2023-10-10 21:15:46 +0700

    fix bug pictures not show in display

--- dasar/pertemuan-5/tugas.php
+++ dasar/pertemuan-5/tugas.php
@@ -22,72 +22,72 @@
       "stok" => "20",
       "warna" => "hijau",
       'harga' => '6.500',
-      "gambar" => "",
+      "gambar" => "pulpen.jpg",
     ],
 
-    // [
-    //   "nama" => "pensil warna",
-    //   "stok" => "40",
-    //   "warna" => "banyak",
-    //   'harga' => '50.000',
-    //   "gambar" => "",
-    // ],
+    [
+      "nama" => "pensil warna",
+      "stok" => "40",
+      "warna" => "banyak",
+      'harga' => '50.000',
+      "gambar" => "pensil warna.jpg",
+    ],
 
-    // [
-    //   "nama" => "kertas binder",
-    //   "stok" => "10",
-    //   "warna" => "putih",
-    //   'harga' => '35.000',
-    //   "gambar" => "",
-    // ],
+    [
+      "nama" => "kertas binder",
+      "stok" => "10",
+      "warna" => "putih",
+      'harga' => '35.000',
+      "gambar" => "kertas binder.jpg",
+    ],
 
-    // [
-    //   "nama" => "buku tulis",
-    //   "stok" => "10",
-    //   "warna" => "biru",
-    //   'harga' => '25.000',
-    //   "gambar" => "",
-    // ],
+    [
+      "nama" => "buku tulis",
+      "stok" => "10",
+      "warna" => "biru",
+      'harga' => '25.000',
+      "gambar" => "buku tulis.jpg",
+    ],
 
-    // [
-    //   "nama" => "hvs",
-    //   "stok" => "20",
-    //   "warna" => "putih",
-    //   'harga' => '30.000',
-    //   "gambar" => "",
-    // ],
+    [
+      "nama" => "hvs",
+      "stok" => "20",
+      "warna" => "putih",
+      'harga' => '30.000',
+      "gambar" => "hvs.jpg",
+    ],
     
-    // [
-    //   "nama" => "amplop",
-    //   "stok" => "15",
-    //   "warna" => "putih",
-    //   'harga' => '2.000',
-    //   "gambar" => "",
-    // ],
+    [
+      "nama" => "amplop",
+      "stok" => "15",
+      "warna" => "putih",
+      'harga' => '2.000',
+      "gambar" => "amplop.jpg",
+    ],
 
-    // [
-    //   "nama" => "tinta",
-    //   "stok" => "40",
-    //   "warna" => "merah",
-    //   'harga' => '15.000',
-    //   "gambar" => "",
-    // ],
+    [
+      "nama" => "tinta",
+      "stok" => "40",
+      "warna" => "merah",
+      'harga' => '15.000',
+      "gambar" => "tinta.png",
+    ],
 
-    // [
-    //   "nama" => "penggaris",
-    //   "stok" => "15",
-    //   "warna" => "bening",
-    //   'harga' => '10.000',
-    //   "gambar" => "",
-    // ],
+    [
+      "nama" => "penggaris",
+      "stok" => "15",
+      "warna" => "bening",
+      'harga' => '10.000',
+      "gambar" => "penggaris.jpg",
+    ],
 
-    // [
-    //   "nama" => "stipek",
-    //   "stok" => "15",
-    //   "warna" => "hitam",
-    //   'harga' => '3.000',
-    //   "gambar" => "",
-    // ],
+    [
+      "nama" => "stipek",
+      "stok" => "15",
+      "warna" => "hitam",
+      'harga' => '3.000',
+      "gambar" => "stipek.jpg",
+    ],
   ];
   ?>
 
@@ -95,12 +95,12 @@
 
   <ul>
     <?php foreach ($toko as $t) : ?>
-      <li> <?php echo $t["nama"] ?> </li>
+      <li> <?php echo "Ini Adalah = $t[nama]" ?> </li>
       <li> <?php echo $t["stok"] ?> </li>
       <li> <?php echo $t["warna"] ?> </li>
       <li> <?php echo $t["harga"] ?> </li>
-      <li> <img src="img/<?php $t["gambar"] ?>"></li>
-      <?php echo "</br>" ?>
+      <li> <img src="img/<?php echo $t["gambar"]; ?>"> </li>
+      <?php echo "</br>"?>
     <?php endforeach ?>
   </ul>
 
