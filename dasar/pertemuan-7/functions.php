f62f231094fb3dbc41ac2b9e6923a4eb7bebdeac
Haloio29 <aureliolucio5@gmail.com>
2023-10-13 10:56:59 +0700

    add create & delete (half)

---
+++ dasar/pertemuan-7/functions.php
@@ -0,0 +1,56 @@
+<?php
+
+//Database
+$conn = mysqli_connect(
+  "localhost",    // hostname
+  "root",         // username
+  "",             // password
+  "belajar_php"  // nama database
+);
+
+//Query
+function query($query)
+{
+  global $conn;
+  // query for select mahasiswa databases
+  $result = mysqli_query($conn, $query);
+
+  // untuk cek apakah database udh kesambung ama PHP 
+  // while ($data = mysqli_fetch_assoc($result)){
+  //   var_dump($data);
+  // }
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+  return $rows;
+}
+
+function create($data)
+{
+  global $conn;
+
+  $nim = htmlspecialchars($data['nim']);
+  $nama = htmlspecialchars($data['nama']);
+  $jurusan = htmlspecialchars($data['jurusan']);
+  $email = htmlspecialchars($data['email']);
+
+  $query = "INSERT INTO mahasiswa VALUES ('','$nim','$nama','$jurusan','$email')";
+  mysqli_query($conn, $query);
+
+  return mysqli_affected_rows($conn);
+}
+
+function delete($id)
+{
+  global $conn;
+  mysqli_query($conn, "DELETE FROM mahasiswa WHERE id=$id");
+  return mysqli_affected_rows($conn);
+}
+
+// ambil data (fetch) mahasiswa dari object result
+// mysqli_fetch_row()     // mengembalikan array numerik
+// mysqli_fetch_assoc()   // mengembalikan array associative
+// mysqli_fetch_array()   // bisa fleksibel, kekurangan karena data disajikan double numerik & associative
+// mysqli_fetch_object()  //
--- dasar/pertemuan-7/index.php
+++ dasar/pertemuan-7/index.php
@@ -1,36 +1,27 @@
 <?php
 // Connection Database
-$conn = mysqli_connect(
-  "localhost",    // hostname
-  "root",         // username
-  "",             // password
-  "belajar_php"  // nama database
-);
+require 'functions.php';
 
-// query for select mahasiswa databases
-$result = mysqli_query($conn, "SELECT * FROM mahasiswa");
-// untuk cek apakah database udh kesambung ama PHP 
-// while ($data = mysqli_fetch_assoc($result)){
-//   var_dump($data);
-// }
-
-// ambil data (fetch) mahasiswa dari object result
-// mysqli_fetch_row()     // mengembalikan array numerik
-// mysqli_fetch_assoc()   // mengembalikan array associative
-// mysqli_fetch_array()   // bisa fleksibel, kekurangan karena data disajikan double numerik & associative
-// mysqli_fetch_object()  //
+$mahasiswa = query("SELECT * FROM mahasiswa");
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 </head>
+
 <body>
   <h1>Daftar Mahasiswa</h1>
+
+  <a href="crud/create.php">ADD</a>
+
+  <br><br>
+
   <table border="1" cellpadding="10" cellspacing="0">
     <tr>
       <th>No</th>
@@ -40,22 +31,23 @@ $result = mysqli_query($conn, "SELECT * FROM mahasiswa");
       <th>Email</th>
       <th>Action</th>
     </tr>
-    
-    <?php $i=1 ?>
-    <?php while ($row=mysqli_fetch_assoc($result)) : ?>
-    <tr>
-      <td><?php echo $i ?></td>
-      <td><?php echo $row["nim"] ?></td>
-      <td><?php echo $row["nama"] ?></td>
-      <td><?php echo $row["jurusan"] ?></td>
-      <td><?php echo $row["email"] ?></td>
-      <td>
-        <a href="">EDIT</a> |
-        <a href="">DELETE</a>
-      </td>
-    </tr>
-    <?php $i++ ?>
-    <?php endwhile ?>
+
+    <?php $i = 1 ?>
+    <?php foreach ($mahasiswa as $row) : ?>
+      <tr>
+        <td><?php echo $i ?></td>
+        <td><?php echo $row["nim"] ?></td>
+        <td><?php echo $row["nama"] ?></td>
+        <td><?php echo $row["jurusan"] ?></td>
+        <td><?php echo $row["email"] ?></td>
+        <td>
+          <a href="">EDIT</a> |
+          <a href="delete.php?id=<?php $row["id"] ?>">DELETE</a>
+        </td>
+      </tr>
+      <?php $i++ ?>
+    <?php endforeach ?>
   </table>
 </body>
+
 </html>
