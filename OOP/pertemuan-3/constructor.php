37d1d1a2cd0b0cc65cb401c20033ff4538a9a03d
Haloio29 <aureliolucio5@gmail.com>
2023-12-08 20:28:23 +0700

    menambahkan materi object-type

--- OOP/pertemuan-3/constructor.php
+++ OOP/pertemuan-3/constructor.php
@@ -1,1 +1,29 @@
 <?php
+
+class produk
+{
+  public $judul, $penulis, $penerbit, $harga;
+
+  public function __construct($judul, $penulis, $penerbit, $harga)
+  {
+    $this->judul = $judul;
+    $this->penulis = $penulis;
+    $this->penerbit = $penerbit;
+    $this->harga = $harga;
+  }
+
+  public function label()
+  {
+    return "$this->penulis, $this->penerbit";
+  }
+
+  public function sayHelo()
+  {
+    return "Hellow World !!!";
+  }
+}
+
+$produk1 = new produk("Raih Mimpi", "Rahman Darsindo", "Weikom", 50000);
+
+echo $produk1->label();
+echo "<hr>";
