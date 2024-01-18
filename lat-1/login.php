43468df5dc685c17ad9a26eafbb0601bfe1c157d
Haloio29 <aureliolucio5@gmail.com>
2024-01-18 13:12:17 +0700

    add simple login,register,dashboard

---
+++ lat-1/login.php
@@ -0,0 +1,52 @@
+<?php
+  include "service/db.php";
+  session_start();
+
+  $login_message = "";
+
+  if(isset($_SESSION["isLogin"])){
+    header("location: dashboard.php");
+  }
+
+  if (isset($_POST['login'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $result = $db->query($sql);
+
+    if($result->num_rows > 0){
+      $data = $result->fetch_assoc();
+
+      $_SESSION["username"] = $data["username"];
+      $_SESSION["isLogin"] = true;
+      
+      header("location: dashboard.php");
+    } else {
+      $login_message = "Data tidak di temukan";
+    }
+    $db->close();
+  } 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php include "layout/header.html" ?>
+
+  <h3>Login</h3>
+  <i><?= $login_message ?></i>
+  <form action="login.php" method="POST">
+    <input type="text" name="username" placeholder="username">
+    <input type="password" name="password" placeholder="password">
+    <button type="submit" name="login">Login</button>
+  </form>
+
+  <?php include "layout/footer.html" ?>
+</body>
+</html>
---
+++ lat-1/register.php
@@ -0,0 +1,50 @@
+<?php
+  include "service/db.php";
+  session_start();
+
+  $register_message = "";
+
+  if(isset($_SESSION["isLogin"])){
+    header("location: dashboard.php");
+  }
+
+  if (isset($_POST["register"])){
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    try {
+      $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+  
+      if ($db->query($sql)){
+        $register_message = "Anda berhasil membuat akun";
+      } else {
+        $register_message = "Anda gagal membuat akun";
+      }
+    } catch (mysqli_sql_exception) {
+      $register_message = "Username sudah di gunakanan";
+    }
+    $db->close();
+  } 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php include "layout/header.html" ?>
+
+  <h3>REGISTER</h3>
+  <i><?= $register_message; ?></i>
+  <form action="register.php" method="POST">
+    <input type="text" name="username" placeholder="username">
+    <input type="password" name="password" placeholder="password">
+    <button type="submit" name="register">Daftar</button>
+  </form>
+
+  <?php include "layout/footer.html" ?>
+</body>
+</html>
